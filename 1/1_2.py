2530e78d0227686c51601f0b780e5710b9ef33d1
Marci Jakab <jakabxmarci@gmail.com>
2022-12-01 14:09:01 +0100

    solution to 1

--- 1/1_2.py
+++ 1/1_2.py
@@ -1,13 +1,14 @@
-max = 0
+max = [0,0,0]
 elf_calories = 0
 
 with open ('input.txt', 'r') as f:
-    for line in f: 
+    for line in f:
         if (line == '\n'):
-            if (max < elf_calories):
-                max = elf_calories
+            max.sort()
+            if max[0] < elf_calories:
+                max[0] = elf_calories
             elf_calories = 0
         else:
             elf_calories += int(line.strip())
         
-print(max)     
+print(sum(max))     
