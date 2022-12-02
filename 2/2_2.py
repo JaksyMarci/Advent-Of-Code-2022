9898ce581ce6de883b20f8433e19b180753fc688
Marci Jakab <jakabxmarci@gmail.com>
2022-12-02 14:56:17 +0100

    solution to 2_2

---
+++ 2/2_2.py
@@ -0,0 +1,31 @@
+
+#A for Rock, B for Paper, and C for Scissors
+
+points = 0
+extrapoints = [1,2,3] #1 for Rock, 2 for Paper, and 3 for Scissors
+startpos = {
+    'A' : 0,
+    'B' : 1,
+    'C' : 2
+}
+shift = {
+    'X' : -1,
+    'Y' : 0,
+    'Z' : -2 #X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+}
+
+shapes = { #0 if you lost, 3 if the round was a draw, and 6 if you won
+    'X' : 0,
+    'Y' : 3,
+    'Z' : 6
+}
+
+with open('input.txt', 'r') as f:
+    for x in f:
+        pair = tuple(x.strip().split(' '))
+        points += extrapoints[ startpos[pair[0]] + shift[pair[1]] ] 
+        points += shapes[pair[1]]
+        
+print(points)
+
+
