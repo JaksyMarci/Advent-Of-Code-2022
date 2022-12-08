70bc032217afed7cc8ac130c44de71a665ca1a38
Marci Jakab <jakabxmarci@gmail.com>
2022-12-08 13:53:06 +0100

    solution to 5_2

---
+++ 5/5_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+STACKS_MAX_HEIGHT = 8
+
+crates = []
+buffer = []
+index = 0
+with open('input.txt', 'r') as f:
+    for line in f:
+        if (index < STACKS_MAX_HEIGHT):
+            #read input stacks
+            crates.append([*line[1::4]])
+            index+=1
+        elif (index == STACKS_MAX_HEIGHT):
+            #arrange data
+            crates = np.fliplr(np.transpose(crates))
+            crates = list(map(lambda elem : list(elem[elem != ' ']), crates)) 
+            index+=1
+        elif (index < STACKS_MAX_HEIGHT + 2): #skip all remaining lines until instructions
+            index+=1
+            print(crates)
+            continue
+        else:
+            #execute instructions
+            instructions = tuple(line.strip().replace('move', ' ').replace('from',' ').replace('to',' ').replace('  ', '').split(' '))
+            for i in range(int(instructions[0])):
+                buffer.append(crates[int(instructions[1]) -1].pop())
+            
+            buffer.reverse()
+            crates[int(instructions[2]) -1].extend(buffer)
+            buffer.clear()
+            
+for x in crates:
+    buffer.append(x.pop()) if len(x) > 0 else buffer.append('')
+print(''.join(buffer))
